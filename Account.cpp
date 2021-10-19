a42f405ce7ab3961f7e740f92631e5e9f2158c84
Rodrigo Becerril Ferreyra <becerrilferreyra.rodrigo@gmail.com>
2021-10-18 21:28:49 -0700

    Added load and save function implementations

--- Account.cpp
+++ Account.cpp
@@ -6,6 +6,7 @@
  * @version 1
  */
 
+#include <string>
 #include <fstream>
 #include "Account.h"
 
@@ -14,6 +15,8 @@ Account::NumOutOfBounds::NumOutOfBounds(int lower, int upper)
     lowerbounds = lower;
     upperbounds = upper;
 }
+
+Account::Account() : Account(10000000, 0){}
 
 Account::Account(int accountNumber, int balance)
 {
@@ -29,5 +32,51 @@ Account::Account(int accountNumber, int balance)
     this->balance = balance;
 }
 
-// this constructor simply calls the other constructor with default values
-Account::Account() : Account(10000000, 0){}
+void Account::load()
+{
+    // build filename
+    std::string filename = "acc_";
+    filename += std::to_string(accountNumber);
+    filename += ".txt";
+
+    // open file
+    std::fstream infile(filename.c_str(), std::ios::in);
+    if(infile.fail())
+        throw FileNotFoundError();
+    
+    // load all values into variables
+    std::string lines[5] = {};
+
+    for(int i = 0; i < 5; ++i)
+        std::getline(infile, lines[i]);
+    
+    // cast string into appropriate type
+    accountNumber = std::stoi(lines[0].substr(16));
+    balance       = std::stod(lines[1].substr(18));
+    gamesPlayed   = std::stoi(lines[2].substr(13));
+    amountWon     = std::stod(lines[3].substr(19));
+    amountLost    = std::stod(lines[4].substr(20));
+
+    infile.close();
+
+}
+
+void Account::save() const
+{
+    // build filename
+    std::string filename = "acc_";
+    filename += std::to_string(accountNumber);
+    filename += ".txt";
+
+    // open file
+    std::fstream outfile(filename.c_str(), std::ios::out);
+
+    // populate file
+    outfile << "Account Number: " << accountNumber << "\n";
+    outfile << "Current balance: $" << balance << "\n";
+    outfile << "Game played: " << gamesPlayed << "\n";
+    outfile << "Total amount won: $" << amountWon << "\n";
+    outfile << "Total amount lost: $" << amountLost << "\n";
+
+    outfile.close();
+}
