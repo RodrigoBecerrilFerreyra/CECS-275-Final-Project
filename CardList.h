d362bfc7f01445a2891ad6bd6dfc7318116f743e
Rodrigo Becerril Ferreyra <becerrilferreyra.rodrigo@gmail.com>
2021-10-31 18:01:29 -0700

    Initialized CardList header file

---
+++ CardList.h
@@ -0,0 +1,22 @@
+/**
+ * CardList class header file.
+ * CECS 275 - Fall 2021
+ * @author Rodrigo Becerril Ferreyra
+ * @author Ethan Hua
+ * @version 1
+ */
+
+#ifndef CARDLIST_H
+#define CARDLIST_H
+
+/**
+ * CardList is a singly-linked list class that can store all of the cards in a
+ * standard French 52-card deck. It implements several functions including
+ * shuffle, which randomizes the order of the deck.
+ */
+class CardList
+{
+
+};
+
+#endif//CARDLIST_H
