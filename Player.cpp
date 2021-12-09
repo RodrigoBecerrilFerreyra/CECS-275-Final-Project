741a6dd941cce600f8dbeecfeb3ae068b1e9f054
Ethan Hua <48600492+sparkslinerhigh@users.noreply.github.com>
2021-12-08 17:20:08 -0800

    Created Player.cpp
    Began working on implementation of Player class.

---
+++ Player.cpp
@@ -0,0 +1,11 @@
+/**
+ * Player class implementation file.
+ * CECS 275 - Fall 2021
+ * @author Rodrigo Becerril Ferreyra
+ * @author Ethan Hua
+ * @version 1
+ */
+
+#include "CardList.h"
+#include "Account.h"
+#include "Player.h"
